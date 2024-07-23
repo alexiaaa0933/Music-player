8d4fce7060f31766b54cf5bfe52bc099dff575d8
German Maria-Alexia <maria.german@student.unitbv.ro>
2024-07-23 20:41:59 +0300

    Display List of Available Songs

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayListComponent } from './components/display-list/display-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: 
+  [
+    AppComponent,
+    DisplayListComponent
   ],
   imports: [
     BrowserModule,

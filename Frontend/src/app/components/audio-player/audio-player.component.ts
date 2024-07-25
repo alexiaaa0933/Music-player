5cc16b68c8d1ccb0960edb83ba98a25654581416
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2024-07-25 11:30:11 +0300

    Added more songs with metadata

--- Frontend/src/app/components/audio-player/audio-player.component.ts
+++ Frontend/src/app/components/audio-player/audio-player.component.ts
@@ -6,54 +6,53 @@ import { Component } from '@angular/core';
   styleUrl: './audio-player.component.scss'
 })
 export class AudioPlayerComponent {
-  buttonPlay:boolean=true;
-  duration:number=60;
-  durationToDisplay:string='';
-  valueToDisplay:string='';
-  min:number=0;
-  value:number=0;
-  start:number=0;
-  interval:any;
-  
-onPlay():void{
-this.buttonPlay=!this.buttonPlay;
-console.log(this.durationToDisplay)
-var timeDiff=this.duration-this.start;
-this.durationToDisplay=this.durationToString(this.duration);
-timeDiff/=1000;
+  buttonPlay: boolean = true;
+  duration: number = 60;
+  durationToDisplay: string = '';
+  valueToDisplay: string = '';
+  min: number = 0;
+  value: number = 0;
+  start: number = 0;
+  interval: any;
 
-if(!this.buttonPlay)
-{console.log(this.value);
-  this.interval=setInterval(()=>
-  { this.value=parseFloat((this.value +1).toFixed(1));
-    this.valueToDisplay=this.durationToString(this.value);
-  if(this.value>=this.duration)
-{
-  this.value=this.duration;
-  this.valueToDisplay=this.durationToString(this.value);
-  this.stop();
- 
-}},1000);
-}
+  onPlay(): void {
+    this.buttonPlay = !this.buttonPlay;
+    console.log(this.durationToDisplay)
+    var timeDiff = this.duration - this.start;
+    this.durationToDisplay = this.durationToString(this.duration);
+    timeDiff /= 1000;
 
-}
-stop():void{
-  if(this.interval)
-  {console.log(this.value);
-    clearInterval(this.interval);
-    this.interval=null;
+    if (!this.buttonPlay) {
+      console.log(this.value);
+      this.interval = setInterval(() => {
+        this.value = parseFloat((this.value + 1).toFixed(1));
+        this.valueToDisplay = this.durationToString(this.value);
+        if (this.value >= this.duration) {
+          this.value = this.duration;
+          this.valueToDisplay = this.durationToString(this.value);
+          this.stop();
+
+        }
+      }, 1000);
+    }
+
   }
-}
-onInputChange(event:any):void
-{
-this.value=event.value;
-console.log(this.value);
-}
+  stop(): void {
+    if (this.interval) {
+      console.log(this.value);
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+  onInputChange(event: any): void {
+    this.value = event.value;
+    console.log(this.value);
+  }
 
-durationToString(duration:number):string{
-  const minutes = Math.floor(duration / 60);
-  const seconds = duration % 60;
-  return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  
-}
+  durationToString(duration: number): string {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+  }
 }
